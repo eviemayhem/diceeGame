7e52c46edc6b0f630291f5a06354d4e4b67c76fe
Eevie <61148035+EevieIles@users.noreply.github.com>
2020-03-18 11:27:07 -0400

    Add files via upload
    Corrected malfunctioning code, edited winner notifications. End product.

--- index.js
+++ index.js
@@ -2,5 +2,67 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber1);
 
 if (randomNumber1 === 1){
-document.getElementById("img1").src = "dice1.png";
+document.getElementById("img1").src = "images/dice1.png";
 }
+
+if (randomNumber1 === 2){
+document.getElementById("img1").src = "images/dice2.png";
+}
+
+if (randomNumber1 === 3){
+document.getElementById("img1").src = "images/dice3.png";
+}
+
+if (randomNumber1 === 4){
+document.getElementById("img1").src = "images/dice4.png";
+}
+
+if (randomNumber1 === 5){
+document.getElementById("img1").src = "images/dice5.png";
+}
+
+if (randomNumber1 === 6){
+document.getElementById("img1").src = "images/dice6.png";
+}
+
+// Player Two Dice
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber2);
+
+if (randomNumber2 === 1){
+document.getElementById("img2").src = "images/dice1.png";
+}
+
+if (randomNumber2 === 2){
+document.getElementById("img2").src = "images/dice2.png";
+}
+
+if (randomNumber2 === 3){
+document.getElementById("img2").src = "images/dice3.png";
+}
+
+if (randomNumber2 === 4){
+document.getElementById("img2").src = "images/dice4.png";
+}
+
+if (randomNumber2 === 5){
+document.getElementById("img2").src = "images/dice5.png";
+}
+
+if (randomNumber2 === 6){
+document.getElementById("img2").src = "images/dice6.png";
+}
+
+// winner code
+
+if (randomNumber1 > randomNumber2){
+  document.getElementById("header").innerHTML = "Player One Wins!";
+}
+
+if (randomNumber1 < randomNumber2){
+  document.getElementById("header").innerHTML = "Player Two Wins!";
+}
+ if (randomNumber1 === randomNumber2){
+   document.getElementById("header").innerHTML = "It's a draw!";
+ }
